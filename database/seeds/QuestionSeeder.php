962f940d6b1f1b1ba194b8c2a57bde440d810363
oscar <oscar_zen17@hotmail.com>
2020-07-25 17:22:12 -0500

    fixed seed

--- database/seeds/QuestionSeeder.php
+++ database/seeds/QuestionSeeder.php
@@ -17,88 +17,86 @@ class QuestionSeeder extends Seeder
         $question = [
             [
                 'question' => 'Nombre completo',
-               
+
             ],
             [
                 'question' => 'Teléfono',
-                
+
             ],
             [
                 'question' => 'Correo electrónico',
-                
+
             ],
             [
                 'question' => 'Puesto',
-               
+
             ],
             [
-                'question' => '¿Según su criterio cuáles son los 2 problemas más relevantes que deberían ser resueltos
-                de manera inmediata?',
-                
+                'question' => '¿Según su criterio cuáles son los 2 problemas más relevantes que deberían ser resueltos de manera inmediata?',
+
             ],
             [
-                'question' => '¿¿Cuenta con documentación que describa la organización de su empresa?',
-                
+                'question' => '¿Cuenta con documentación que describa la organización de su empresa?',
+
             ],
             [
                 'question' => ' ¿Cuáles documentos son?',
-                
+
             ],
             [
                 'question' => '¿Los empleados conocen estos documentos?',
-                
+
             ],
             [
                 'question' => '¿Los empleados tienen acceso a estos documentos?',
-                
+
             ],
             [
                 'question' => '¿Cada cuando aplican herramientas para evaluar el ambiente de trabajo?',
-                
+
             ],
             [
                 'question' => '¿Cuál es la frecuencia con la que los empleados se capacitan?',
-                
+
             ],
             [
-                'question' => '¿Conoce o se tiene registro de las leyes, reglamentos y normas a las cuales se tiene que
-                alinear la empresa?',
-                
+                'question' => '¿Conoce o se tiene registro de las leyes, reglamentos y normas a las cuales se tiene que alinear la empresa?',
+
             ],
             [
                 'question' => '¿Cuáles son?',
-                
+
             ],
             [
                 'question' => '¿Según su criterio, en cuales no se está cumpliendo la norma/reglamento/ley?',
-                
+
             ],
             [
                 'question' => '¿Conoce el estado financiero actual de la empresa?',
-                
+
             ],
             [
                 'question' => '¿Cada cuando se actualiza la información financiera para poder ser consultada?',
-                
+
             ],
             [
                 'question' => '¿Qué herramientas de software utilizan para gestionar la información financiera?',
-                
+
             ],
             [
                 'question' => 'El software que utiliza la empresa ¿Qué porcentaje cuenta con licencia vigente?',
-                
+
             ],
             [
                 'question' => 'Anotaciones y observaciones del consultor',
-                
+
             ],
 
-            
+
         ];
 
 
-        foreach ($question as $key => $squestion) {            
+        foreach ($question as $key => $squestion) {
             $question = new Question();
             $question->question = $squestion['question'];
             $question->status = 1;
